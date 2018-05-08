cfdec7139eb8b2ac2e510cec4e59425ed505741e
MaxAbrosimov <maximabrosimovkh@gmail.com>
2018-05-08 13:18:49 +0300

    Change cassandra config to Durable Writes

--- src/main/java/backend/config/CassandraConfig.java
+++ src/main/java/backend/config/CassandraConfig.java
@@ -5,9 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,14 +23,11 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
 
-        return Collections.singletonList(specification);
-    }
-
-    @Override
-    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
-        return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
+        return Collections.singletonList(CreateKeyspaceSpecification.createKeyspace(KEYSPACE)
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication());
     }
 
     @Override
